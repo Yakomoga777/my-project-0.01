4f7f436e551200a1ae724d77448c403e9e27a4c7
Yakomoga777 <112173106+Yakomoga777@users.noreply.github.com>
2024-08-20 19:40:31 +0000

    Deploying to gh-pages from @ Yakomoga777/my-project-0.01@a5af66fa0b06e0655fc1fcf08bae470cc08fb001 🚀

---
+++ index.7b0d7da6.js
@@ -0,0 +1,2 @@
+!function(){var n=document.querySelector("button"),o=document.querySelector(".form__input");console.log(o),n.addEventListener("click",(function(){console.log("buttonClick")})),o.addEventListener("click",(function(){console.log("inputClick")}))}();
+//# sourceMappingURL=index.7b0d7da6.js.map
