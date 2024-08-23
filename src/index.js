2b34cf19ce41fff79c0a54ed69931aa57b7cffaf
Roman Petrenko <rpetrenko0105@gmail.com>
2024-08-23 21:32:13 +0200

    add listener
    reads the area valey and add to array

--- src/index.js
+++ src/index.js
@@ -1,16 +1,29 @@
-const buttonEl = document.querySelector('button')
-const inputEl = document.querySelector('.form__input')
-console.log(inputEl);
+const buttonEl = document.querySelector('button');
+const textareaEL = document.querySelector('.form__textarea--ask');
+// console.log(inputEl);
 
+buttonEl.addEventListener('click', onButtonClick);
+textareaEL.addEventListener('click', onInputClick);
 
-buttonEl.addEventListener('click', onButtonClick)
-inputEl.addEventListener('click', onInputClick)
+let vocabulary = [];
 
-function onButtonClick() {
-    console.log('buttonClick');
+// const newWord = () => {
+//   return textareaEL.value;
+// };
+
+const addWord = (word) => {
+  //1. Зчитаємо слово з поля
+  const newWord = textareaEL.value;
+  // newWord();
+
+  //2. Додаємо новий масив до головного існуючого
+  console.log(vocabulary);
+  return vocabulary.push(newWord);
+};
+
+function onButtonClick(event) {
+  addWord();
 }
 function onInputClick() {
-    console.log('inputClick');
+  console.log('inputClick');
 }
-
-
